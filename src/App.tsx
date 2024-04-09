46d3c2d1dd954e7813d2b2a115ad539fccf45e5f
Kreoniz <kreoniz@mail.ru>
2024-04-09 09:32:53 +0600

    Add prettier and format code

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,63 @@
-import Header from "./components/Header.tsx"
-import './App.css'
-import Post from "./components/Post.tsx"
+import Header from "./components/Header.tsx";
+import "./App.css";
+import Post from "./components/Post.tsx";
 import coolAva from "./assets/ip5k_G-4Zh0.jpg";
 
 type Props = {
   userName: string;
-}
+};
 
 function App(props: Props) {
-  
   return (
     <>
       <Header img={coolAva} userName={props.userName} />
       <div className="postContainer">
-        <Post autor={props.userName} title= "Artificial Intelligence: The Future Is Here - How AI is Changing the World 🤖🌐" text={"1"} date={new Date(2023,7,18)}/>
-        <Post autor={props.userName} title= "Artificial Intelligence: The Future Is Here - How AI is Changing the World 🤖🌐" text={ "ABERTODELSngjkdslkjbgljkabdl aSSFKJ BDKLBGLK K;JJNSNAKJ FKDBB LBA FJKAS KJNFAJK NDJKGB KJLABJK DGDJKN DJNA N ASKN"+ "1"} date={new Date(2023,7,18)}/>
-        <Post autor={props.userName} title= "Artificial Intelligence: The Future Is Here - How AI is Changing the World 🤖🌐" text={ "ABERTODELSngjkdslkjbgljkabdl aSSFKJ BDKLBGLK K;JJNSNAKJ FKDBB LBA FJKAS KJNFAJK NDJKGB KJLABJK DGDJKN DJNA N ASKN"+ "1"} date={new Date(2023,7,18)}/>
-        <Post autor={props.userName} title= "Artificial Intelligence: The Future Is Here - How AI is Changing the World 🤖🌐" text={ "ABERTODELSngjkdslkjbgljkabdl aSSFKJ BDKLBGLK K;JJNSNAKJ FKDBB LBA FJKAS KJNFAJK NDJKGB KJLABJK DGDJKN DJNA N ASKN"+ "1"} date={new Date(2023,7,18)}/>
-        <Post autor={props.userName} title= "Artificial Intelligence: The Future Is Here - How AI is Changing the World 🤖🌐" text={ "ABERTODELSngjkdslkjbgljkabdl aSSFKJ BDKLBGLK K;JJNSNAKJ FKDBB LBA FJKAS KJNFAJK NDJKGB KJLABJK DGDJKN DJNA N ASKN"+ "1"} date={new Date(2023,7,18)}/>
-
+        <Post
+          autor={props.userName}
+          title="Artificial Intelligence: The Future Is Here - How AI is Changing the World 🤖🌐"
+          text={"1"}
+          date={new Date(2023, 7, 18)}
+        />
+        <Post
+          autor={props.userName}
+          title="Artificial Intelligence: The Future Is Here - How AI is Changing the World 🤖🌐"
+          text={
+            "ABERTODELSngjkdslkjbgljkabdl aSSFKJ BDKLBGLK K;JJNSNAKJ FKDBB LBA FJKAS KJNFAJK NDJKGB KJLABJK DGDJKN DJNA N ASKN" +
+            "1"
+          }
+          date={new Date(2023, 7, 18)}
+        />
+        <Post
+          autor={props.userName}
+          title="Artificial Intelligence: The Future Is Here - How AI is Changing the World 🤖🌐"
+          text={
+            "ABERTODELSngjkdslkjbgljkabdl aSSFKJ BDKLBGLK K;JJNSNAKJ FKDBB LBA FJKAS KJNFAJK NDJKGB KJLABJK DGDJKN DJNA N ASKN" +
+            "1"
+          }
+          date={new Date(2023, 7, 18)}
+        />
+        <Post
+          autor={props.userName}
+          title="Artificial Intelligence: The Future Is Here - How AI is Changing the World 🤖🌐"
+          text={
+            "ABERTODELSngjkdslkjbgljkabdl aSSFKJ BDKLBGLK K;JJNSNAKJ FKDBB LBA FJKAS KJNFAJK NDJKGB KJLABJK DGDJKN DJNA N ASKN" +
+            "1"
+          }
+          date={new Date(2023, 7, 18)}
+        />
+        <Post
+          autor={props.userName}
+          title="Artificial Intelligence: The Future Is Here - How AI is Changing the World 🤖🌐"
+          text={
+            "ABERTODELSngjkdslkjbgljkabdl aSSFKJ BDKLBGLK K;JJNSNAKJ FKDBB LBA FJKAS KJNFAJK NDJKGB KJLABJK DGDJKN DJNA N ASKN" +
+            "1"
+          }
+          date={new Date(2023, 7, 18)}
+        />
       </div>
       <></>
     </>
-  )
+  );
 }
 
-
-export default App
+export default App;
--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,21 +1,21 @@
-import "./Post.css"
+import "./Post.css";
 
 type Props = {
-    autor: string,
-    title: string;
-    text: any;
-    date: Date;
+  autor: string;
+  title: string;
+  text: any;
+  date: Date;
+};
+
+function Post(props: Props) {
+  return (
+    <div className="post">
+      <h3 className="postTitle">{props.title}</h3>
+      <h4 className="postAutor">{props.autor}</h4>
+      <p className="postText">{props.text}</p>
+      <p className="postDate">{props.date.toDateString()}</p>
+    </div>
+  );
 }
 
-function Post(props : Props) {
-    return (
-        <div className="post">
-            <h3 className="postTitle">{props.title}</h3>
-            <h4 className="postAutor">{props.autor}</h4>
-            <p className="postText">{props.text}</p>
-            <p className="postDate">{props.date.toDateString()}</p>
-        </div>
-    );
-}
-
-export default Post
+export default Post;
